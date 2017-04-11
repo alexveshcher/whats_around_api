d23f718fba39618a60bd0765ef717d7a1d822446
Oleksandr Veshcherovych <s.veshcherovych@gmail.com>
2017-04-11 12:46:28 +0300

    can find places by category(sector)

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -20,6 +20,7 @@ class TestsController < ApplicationController
     venues.each do |v|
       id = v['id']
       name = v['name']
+      address = v['location']['address']
       lat = v['location']['lat']
       lng = v['location']['lng']
       distance = v['location']['distance']
@@ -30,19 +31,72 @@ class TestsController < ApplicationController
         categs << Category.new(c['id'], c['name'])
       end
 
-      places << Place.new(id, name, lat, lng, distance, categs)
+      places << Place.new(id, name, address, lat, lng, distance, categs)
     end
 
     render json: places
   end
+
+
+  def find_by_section
+    # params.permit(:lat, :lng, :categories)
+    p_latitude = params[:lat]
+    p_longitude = params[:lng]
+    p_section = params[:section]
+
+
+
+    url = 'https://api.foursquare.com/v2/venues/explore'\
+    "?ll=#{p_latitude},#{p_longitude}"\
+    '&client_id=NU54NGRVGGQQ2BDSTBGWVQ3LLP44USMS3AP4A1IBQXYFG5RD'\
+    '&client_secret=SD3LOHWHYN04KDUU0CB1HPNASPXTKAKB10QJQZTJ1PMDYWST'\
+    '&v=20170405'\
+    '&radius=15000'\
+    "&section=#{p_section}"
+    puts url
+    response = Net::HTTP.get_response(URI.parse(url))
+
+
+
+    obj = JSON.parse(response.body)
+    venues = []
+    obj['response']['groups'].first['items'].each do |i|
+      venues << i['venue']
+    end
+    # venues = obj['response']['venues']
+
+    places = []
+    venues.each do |v|
+      id = v['id']
+      name = v['name']
+      address = v['location']['address']
+      lat = v['location']['lat']
+      lng = v['location']['lng']
+      distance = v['location']['distance']
+      categories = v['categories']
+
+      categs = []
+       categories.each do |c|
+        categs << Category.new(c['id'], c['name'])
+      end
+      new_place = Place.new(id, name, address, lat, lng, distance, categs)
+      # puts new_place.inspect
+      places << new_place
+    end
+
+    # render JSON.pretty_generate(some_data)
+    render json: places
+
+  end
 end
 
 class Place
-  attr_accessor :id, :name, :lat, :lng, :distance, :categories
+  attr_accessor :id, :name, :address, :lat, :lng, :distance, :categories
 
-  def initialize(id, name, lat, lng, distance, categories)
+  def initialize(id, name, address, lat, lng, distance, categories)
     @id = id
     @name = name
+    @address = address
     @lat = lat
     @lng = lng
     @distance = distance
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/tests/try/:lat/:lng', to: 'tests#try', :constraints => { :lat => /[^\/]+/ , :lng => /[^\/]+/ }
+  get '/api/v1/places/:lat/:lng/:section', to: 'tests#find_by_section', :constraints => { :lat => /[^\/]+/ , :lng => /[^\/]+/ , :sectiion => /[^\/]+/ }
 end
